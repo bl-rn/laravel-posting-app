928daaa370f2ec0e5091a8e90703f497c7ec26e2
RN <bestlife.nagairyo@gmail.com>
2024-12-12 11:36:47 +0900

    10.2 Let's check the completed form

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -18,8 +18,8 @@ class PostFactory extends Factory
     {
         return [
             'user_id' => '1',
-            'title' => fake()->realText(20),
-            'content' => fake()->realText(100)
+            'title' => fake()->realText(20, 5),
+            'content' => fake()->realText(100, 5)
         ];
     }
 }
